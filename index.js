0c2ddee0b669c6a51f3ab4763b42963420412533
Precious <prechieb@gmail.com>
2021-03-06 14:18:48 +0000

    simple modification

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict";
 
+//image slider 
 let slideIndex = 0;
+let firstSearch;
 
 
 function next(){
@@ -31,3 +33,67 @@ showSlide();
 setInterval(() => {
     next();
 }, 5000);
+
+//close recepi page
+
+let recipeBox = document.getElementById("recipe_box");
+
+function closeRecipe() {
+    recipeBox.style.display = "none";
+}
+
+
+//API Search 
+
+function searchBtn(e) {
+
+   e.preventDefault();
+   
+    let html = document.getElementById("food_containner");
+    html.innerHTML = "";
+
+    let foodName =  document.querySelector("#search_box").value;
+
+    if(foodName){ 
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s= ${foodName}`)
+        .then(response => response.json())
+        .then(data => {
+           
+    
+            if(data.meals){
+                data.meals.forEach(element => {
+    
+                    html.innerHTML +=  `<div class="food">
+                    <img src=${element.strMealThumb} alt="first food"/>
+                    <h5>${element.strMeal}</h5>
+                <input type="submit" value="see recipe" onclick = seeRecipe()>
+                 </div>`
+                 recipeBox.children[1].innerHTML = element.strMeal;
+                 recipeBox.children[2].innerHTML = element.strInstructions;
+                });
+    
+               
+            }
+    
+           
+        })
+    }
+  
+}
+
+//Recipe Details
+
+function seeRecipe(){
+    recipeBox.style.display = "block";
+}
+
+//Homescreen redirect 
+
+
+function firstInput(){
+    firstSearch = document.querySelector("#first_search_box").value;
+
+    console.log()
+
+}
+
